e7ad08df2e3a05f969bc205f594c63205475c477
octaaja <86885864+octaaja@users.noreply.github.com>
2022-09-14 03:19:06 +0700

    password edit2

--- hyunjun/password.js
+++ hyunjun/password.js
@@ -1,10 +1,10 @@
 var password;
 var pass1="20220514";
 
-password=prompt('Do you remember the first day we met?',' ');
+password=prompt('처음 만났던 날',' ');
 
 if (password==pass1)
-  alert('갬동!');
+  alert('싫어요!');
 else
    {
     window.location="https://static.wikia.nocookie.net/spongebob/images/9/9a/Wishing_You_Well_068.png/revision/latest/scale-to-width-down/1000?cb=20191023005828";
