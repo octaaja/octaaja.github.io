b1d0a1dca78ab48f4c5be84bc4d29b1c5e166567
octaaja <86885864+octaaja@users.noreply.github.com>
2022-09-19 17:34:52 +0700

    password changes

--- hyunjun/password1.js
+++ hyunjun/password1.js
@@ -4,7 +4,7 @@ var pass1="유니corn 누나";
 password=prompt('세상에서 가장 예쁜?',' ');
 
 if (password==pass1)
-  alert('싫어요!');
+  alert('THANKS!');
 else
    {
     window.location="https://i.imgflip.com/26931v.jpg";
