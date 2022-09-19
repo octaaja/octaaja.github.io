534536f16f0e0838ee5c1ec880198fdaa6af7896
octaaja <86885864+octaaja@users.noreply.github.com>
2022-09-19 17:28:25 +0700

    password addition

---
+++ hyunjun/password1.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="유니corn 누나";
+
+password=prompt('세상에서 가장 예쁜?',' ');
+
+if (password==pass1)
+  alert('싫어요!');
+else
+   {
+    window.location="https://i.imgflip.com/26931v.jpg";
+    }
---
+++ hyunjun/password2.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="20220514";
+
+password=prompt('처음 만났던 날',' ');
+
+if (password==pass1)
+  alert('싫어요!');
+else
+   {
+    window.location="https://i.imgflip.com/26931v.jpg";
+    }
