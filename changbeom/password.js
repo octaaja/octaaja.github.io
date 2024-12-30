e0cf2de6c742bb9c5ebfcf0a23f0fedfe5b85237
octaaja <linenpod@gmail.com>
2024-12-30 14:09:33 +0700

    test

---
+++ changbeom/password.js
@@ -0,0 +1,9 @@
+var password;
+var pass1=" 20240830";
+
+password=prompt('처음 만났던 날',' ');
+
+if (password==pass1)
+  alert('참 잘했어요!');
+else
+   {window.location="https://jjalbang.today/files/jjalbox/2023/04/20230414_64395fbfa02bc.jpg";}
