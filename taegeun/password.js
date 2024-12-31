10d09735fc365be199188cfa587e19364be80313
octaaja <86885864+octaaja@users.noreply.github.com>
2024-12-31 15:31:56 +0700

    final i guess

--- taegeun/password.js
+++ taegeun/password.js
@@ -4,6 +4,6 @@ var pass1=" 20241004";
 password=prompt('처음 만났던 날',' ');
 
 if (password==pass1)
-  alert('요요ㅎㅎ');
+  alert('요오오오오오ㅎㅎ');
 else
    {window.location="https://i.imgflip.com/26931v.jpg";}
